6f4370350b7bf7a2b1f3264053eec8fda8804945
Dawid <dawdom34@wp.pl>
2023-10-01 22:04:26 +0200

    Added signal to delete image from server if image object was deleted

--- images/models.py
+++ images/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 from users.models import Account
 
@@ -31,3 +33,11 @@ class Image(models.Model):
     thumbnails = models.ManyToManyField(Thumbnail, help_text='Thumbnails created from original image', blank=True, null=True)
     owner = models.ForeignKey(Account, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=images_filepath, blank=False, null=False)
+
+
+@receiver(pre_delete, sender=Image)
+def delete_file(sender, instance, **kwargs):
+    """
+    Delete image from server 
+    """
+    instance.image.delete(False)
