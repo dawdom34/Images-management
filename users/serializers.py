4d33f5c906b4e5e1da29c8ec2b946a635b7d34c1
Dawid <dawdom34@wp.pl>
2023-10-01 20:23:21 +0200

    Added create_tier view

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Account
+from .models import Account, AccountTier
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = Account
         fields = ['id', 'username', 'password']
+
+
+class TierSerializer(serializers.ModelSerializer):
+    thumbnail_size = serializers.CharField()
+
+    class Meta:
+        model = AccountTier
+        fields = ['name', 'thumbnail_size', 'original_file', 'expiring_links']
--- users/views.py
+++ users/views.py
@@ -8,8 +8,8 @@ from rest_framework.authentication import TokenAuthentication
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import authenticate
 
-from .models import Account
-from .serializers import UserSerializer
+from .models import Account, AccountTier, ThumbnailSizes
+from .serializers import UserSerializer, TierSerializer
 
 
 @api_view(['POST'])
@@ -31,3 +31,43 @@ def logout_user(request):
     except:
         pass
     return Response({"detail": "Logout successfully"}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+def create_tier(request):
+    """
+    Create new acount tier
+    """
+
+    # Check if user have admin permissions
+    if request.user.is_admin:
+        serialzier = TierSerializer(data=request.data)
+        if serialzier.is_valid():
+            # Save data from serializer
+            tier_name = serialzier.data['name']
+            original_file = serialzier.data['original_file']
+            expiring_links = serialzier.data['expiring_links']
+            # Convert thumbnail_size (string) to list
+            thumbnail_size = serialzier.data['thumbnail_size'].split(',')
+            # Validate data
+            try:
+                sizes = [int(x) for x in thumbnail_size]
+                if 0 in sizes:
+                    return Response({"error": "invalid data type for 'thumbnail_size'"}, status=status.HTTP_400_BAD_REQUEST)
+            except ValueError:
+                return Response({"error": "invalid data type for 'thumbnail_size'"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Create account tier object
+            new_tier = AccountTier.objects.create(name=tier_name, original_file=original_file, expiring_links=expiring_links)
+
+            # Assign thumbnail sizes to new created account tier
+            for size in sizes:
+                new_size = ThumbnailSizes.objects.get_or_create(size=size)
+                new_tier.thumbnail_size.add(new_size[0])
+            
+            return Response({"detail": "New tier created"}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": serialzier.errors}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"detail": "Access denied"}, status=status.HTTP_401_UNAUTHORIZED)
